2b038cd4b0a230d2c713000188055fa7b1300a06
Lemme-cook <238409880+William-Edric-Harianto@users.noreply.github.com>
2026-09-20 23:37:57 +0700

    Update model_course.php

--- model/model_course.php
+++ model/model_course.php
@@ -4,7 +4,7 @@ class model_course
 {
     public $name;
     public $description;
-    public $enrolled_list = array();
+    public $enrolled_list = array();//course baru pasti ga punya student
 
     public function __construct($name, $description)
     {
